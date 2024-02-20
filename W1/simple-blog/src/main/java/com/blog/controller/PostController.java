376ff62dc31ec35a63b3941a694f09b73bffbbaf
delvitord <vitorahim@gmail.com>
2024-02-20 19:54:44 +0700

    first commit

---
+++ W1/simple-blog/src/main/java/com/blog/controller/PostController.java
@@ -0,0 +1,53 @@
+package com.blog.controller;
+
+import com.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.blog.vo.Post;
+
+import java.util.List;
+
+@RestController
+public class PostController {
+    @Autowired
+    PostService postService;
+
+    @GetMapping("/post")
+    public Post getPost(@RequestParam("id") Long id) {
+        Post post = postService.getPost(id);
+        return post;
+    }
+
+    @GetMapping("/post/{id}")
+    public Post getPostPathParam(@PathVariable("id") Long id) {
+        Post post = postService.getPost(id);
+        return post;
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getPosts() {
+        List<Post> posts = postService.getPosts();
+        return posts;
+    }
+
+    @RequestMapping("/posts/updtdate/asc")
+    public List<Post> getPostsOrderByUpdtDateAsc() {
+        List<Post> posts = postService.getPostsOrderByUpdtDateAsc();
+        return posts;
+    }
+
+    @RequestMapping("/posts/regdate/desc")
+    public List<Post> getPostsOrderByRegDateDesc() {
+        List<Post> posts = postService.getPostsOrderByRegDateDesc();
+        return posts;
+    }
+
+    @RequestMapping("/posts/search/title")
+    public List<Post> searchByTitle(@RequestParam("query") String query) {
+        List<Post> posts = postService.searchPostsByTitle(query);
+        return posts;
+    }
+
+
+}
---
+++ W1/simple-blog/src/main/java/com/blog/service/PostService.java
@@ -0,0 +1,42 @@
+package com.blog.service;
+
+
+import com.blog.repository.PostRepository;
+import com.blog.vo.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostService {
+    private static List<Post> posts;
+    @Autowired
+    PostRepository postRepository;
+    public Post getPost(Long id) {
+        Post post = postRepository.findOne(id);
+
+        return post;
+    }
+
+    public List<Post> getPosts() {
+        List<Post> postList = postRepository.findPosts();
+        return postList;
+    }
+
+    public List<Post> getPostsOrderByUpdtDateAsc() {
+        List<Post> postList = postRepository.findPostOrderByUpdtDateAsc();
+        return postList;
+    }
+
+    public List<Post> getPostsOrderByRegDateDesc() {
+        List<Post> postList = postRepository.findPostOrderByRegDateDesc();
+        return postList;
+    }
+
+    public List<Post> searchPostsByTitle(String query) {
+        List<Post> posts = postRepository.findPostLikeTitle(query);
+        return posts;
+    }
+}
